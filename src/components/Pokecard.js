ca89ac2bc6c83cb909d90583f30bd2f85ab2d9f9
Erik Correa <erikcorrea444@gmail.com>
2021-07-06 23:01:57 -0500

    update to card style

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../styling/Pokecard.css';
 
-const POKE_API = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/`;
+// const POKE_API = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/`;
+
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
 
 const Pokecard = ({id, name, type, exp}) => {
-    let imgSrc = `${POKE_API}${id}.png`;
+    let imgSrc = `${POKE_API}${padToThree(id)}.png`;
     return (
         <div className="pokecard-container">
-            <h1>{name}</h1>
+            <h1 className="pokecard-title">{name}</h1>
             <img src={imgSrc} alt={name}/>
-            <div>Type: {type}</div>
-            <div>EXP: {exp}</div>
+            <div className="pokecard-data">Type: {type}</div>
+            <div className="pokecard-data">EXP: {exp}</div>
         </div>
     );
 }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Pokecard from './Pokecard';
+import '../styling/Pokedex.css';
 
 
 const Pokedex  = () => {
@@ -18,9 +19,11 @@ const Pokedex  = () => {
     return (
         <div className="pokedex-container">
             <h1>Pokedex!</h1>
-            {pokemon.map((p) => (
-                <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-            ))}
+            <div className="pokedex-cards">
+                {pokemon.map((p) => (
+                    <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+                ))}
+            </div>
         </div>
     )
 }
